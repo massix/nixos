e0592e85540d7455eba408491cc9ade0d72c3416
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2023-09-30 13:56:55 +0200

    neovim: start integrating completion engine
    I still have to figure out how to configure the snippet thing
    in a way which could be useable, but for now I'm quite satisfied
    with the result!

--- home/modules/neovim/files/plugins_coding.lua
+++ home/modules/neovim/files/plugins_coding.lua
@@ -203,7 +203,7 @@ return {
     'neovim/nvim-lspconfig',
     event = { 'BufReadPre', 'BufNewFile' },
     cmd = { 'LspInfo' },
-    version = true,
+    version = false,
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.nil_ls.setup {}
@@ -242,7 +242,73 @@ return {
       -- Similar to .vscode things
       { 'folke/neoconf.nvim', cmd = 'Neoconf', config = false, dependencies = { 'nvim-lspconfig' } },
       { 'folke/neodev.nvim', opts = {} },
+      { 'hrsh7th/cmp-nvim-lsp', config = true },
     },
-  }
+  },
+
+  -- completion engine
+  {
+    'hrsh7th/nvim-cmp',
+    version = false,
+    event = 'InsertEnter',
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-cmdline' },
+      { 'hrsh7th/nvim-cmp' },
+      { 'L3MON4D3/LuaSnip' },
+    },
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-Tab>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "cmdline" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("util.defaults").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+  },
 }
 
