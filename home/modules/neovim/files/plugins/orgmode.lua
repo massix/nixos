9f920864ae23fa995441db4824d1c6e7dddb5447
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-01-06 12:20:49 +0100

    nvim: change orgmode default folding

--- home/modules/neovim/files/plugins/orgmode.lua
+++ home/modules/neovim/files/plugins/orgmode.lua
@@ -55,7 +55,7 @@ return {
       org_hide_leading_stars = true,
       org_hide_emphasis_markers = true,
       org_log_into_drawer = "LOGBOOK",
-      org_startup_folded = "inherit",
+      org_startup_folded = "content",
       org_capture_templates = {
         t = {
           description = "Task",
