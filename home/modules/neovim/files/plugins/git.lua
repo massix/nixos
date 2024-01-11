f56001cc375d880d74128cbb9c3c45ec82d510eb
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-01-11 09:50:07 +0100

    nvim: neogit configuration

--- home/modules/neovim/files/plugins/git.lua
+++ home/modules/neovim/files/plugins/git.lua
@@ -6,25 +6,41 @@ local spec = {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",
+      { "sindrets/diffview.nvim", lazy = false, },
+      { "ibhagwan/fzf-lua", lazy = false },
     },
-    config = true,
+    version = false,
     opts = {
+      disable_hint = false,
+      disable_signs = false,
+      disable_line_numbers = false,
       console_timeout = 15000,
       status = {
         recent_commit_count = 50,
       },
+      graph_style = "unicode",
+      signs = {
+        hunk = { " ", " " },
+        item = { " ", " " },
+        section = { " ", " " },
+      },
+      integrations = {
+        telescope = true,
+        diffview = true,
+      },
     },
+    config = function(_, opts)
+      local ng = require("neogit")
+      ng.setup(opts)
 
-    -- Reload Nvimtree automatically
-    init = function()
+      -- Reload Nvimtree automatically
       local group = api.nvim_create_augroup("NeogitEvents", { clear = true })
+
       api.nvim_create_autocmd("User", {
         group = group,
         pattern = "NeogitPushComplete",
         callback = function()
-          require("neogit").close()
+          ng.close()
         end,
       })
     end,
@@ -36,20 +52,12 @@ local spec = {
         end,
         desc = "Open Neogit",
       },
-      {
-        "<leader>gC",
-        function()
-          require("neogit").open({ "commit" })
-        end,
-        desc = "Open Neogit commit",
-      },
     },
   },
 
   -- Git blame info
   {
     "f-person/git-blame.nvim",
-    version = false,
     config = true,
     event = { "BufReadPre", "BufNewFile" },
     keys = {
@@ -60,7 +68,6 @@ local spec = {
   -- Git signs
   {
     "lewis6991/gitsigns.nvim",
-    version = false,
     config = true,
     event = { "BufReadPre", "BufNewFile" },
   },
