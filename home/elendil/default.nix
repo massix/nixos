2b4ac14d263d569cc05a445653a8fc60e431ad5b
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-04 12:23:27 +0100

    feat(elendil): remove foot terminal

--- home/elendil/default.nix
+++ home/elendil/default.nix
@@ -131,50 +131,6 @@ in
       };
     };
 
-    foot = {
-      enable = true;
-      package = unstable.foot;
-      server.enable = true;
-
-      settings = {
-        main = {
-          font = "Monaspace Neon:size=10";
-          dpi-aware = false;
-
-        };
-
-        colors = {
-          background = "24283b";
-          foreground = "c0caf5";
-          regular0 = "1D202F";
-          regular1 = "f7768e";
-          regular2 = "9ece6a";
-          regular3 = "e0af68";
-          regular4 = "7aa2f7";
-          regular5 = "bb9af7";
-          regular6 = "7dcfff";
-          regular7 = "a9b1d6";
-          bright0 = "414868";
-          bright1 = "f7768e";
-          bright2 = "9ece6a";
-          bright3 = "e0af68";
-          bright4 = "7aa2f7";
-          bright5 = "bb9af7";
-          bright6 = "7dcfff";
-          bright7 = "c0caf5";
-        };
-
-        scrollback.lines = 50000;
-        cursor = {
-          style = "beam";
-          blink = true;
-          beam-thickness = 1.5;
-        };
-
-        mouse.hide-when-typing = true;
-      };
-    };
-
     kitty = {
       enable = true;
       package = unstable.kitty;
