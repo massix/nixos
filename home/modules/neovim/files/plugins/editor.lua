838705cabef2753c93f0257c3d7e1619ff961241
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-09 17:07:16 +0100

    fix(nvim): take punctuation into consideration for stop words

--- home/modules/neovim/files/plugins/editor.lua
+++ home/modules/neovim/files/plugins/editor.lua
@@ -871,7 +871,8 @@ return {
     config = function()
       local neowords = require("neowords")
       local p = neowords.pattern_presets
-      local subword_hops = neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.number, p.hex_color)
+      local subword_hops =
+        neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.number, p.hex_color, "\\v\\.+", "\\v,+")
 
       vim.keymap.set({ "n", "x", "o" }, "w", subword_hops.forward_start)
       vim.keymap.set({ "n", "x", "o" }, "e", subword_hops.forward_end)
