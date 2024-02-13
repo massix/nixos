839de8c31d524a3b5cc16312ca91bafcc06bd869
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-02-13 11:09:38 +0100

    fix(nvim): better configuration for neowords

--- home/modules/neovim/files/plugins/editor.lua
+++ home/modules/neovim/files/plugins/editor.lua
@@ -872,7 +872,21 @@ return {
       local neowords = require("neowords")
       local p = neowords.pattern_presets
       local subword_hops =
-        neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.number, p.hex_color, "\\v\\.+", "\\v,+")
+        neowords.get_word_hops(
+        p.snake_case,
+        p.camel_case,
+        p.upper_case,
+        p.number,
+        p.hex_color,
+        "\\v:+",
+        "\\v;",
+        "\\v\\.+",
+        "\\v,+",
+        "\\v\\(+",
+        "\\v\\)+",
+        "\\v\\{+",
+        "\\v\\}+",
+        "\\v$+")
 
       vim.keymap.set({ "n", "x", "o" }, "w", subword_hops.forward_start)
       vim.keymap.set({ "n", "x", "o" }, "e", subword_hops.forward_end)
