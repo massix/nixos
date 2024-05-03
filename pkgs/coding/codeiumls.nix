65bf979a359b9813deb701b19753f2c870221892
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2024-05-03 09:26:47 +0200

    feat(pkgs): codeium -> 1.8.30

--- pkgs/coding/codeiumls.nix
+++ pkgs/coding/codeiumls.nix
@@ -1,22 +1,38 @@
 { unstable, ... }:
-unstable.stdenvNoCC.mkDerivation rec {
+let
+  inherit (unstable) lib;
+  version = "1.8.30";
+  fetchCodeium = version: hash: builtins.fetchurl {
+    url = "https://github.com/Exafunction/codeium/releases/download/language-server-v${version}/language_server_linux_x64.gz";
+    sha256 = hash;
+  };
+in
+unstable.stdenvNoCC.mkDerivation {
   pname = "codeium-ls";
-  version = "1.8.16";
+  inherit version;
 
   nativeBuildInputs = with unstable; [ autoPatchelfHook ];
 
-  src = builtins.fetchurl {
-    url = "https://github.com/Exafunction/codeium/releases/download/language-server-v${version}/language_server_linux_x64.gz";
-    sha256 = "sha256:0qfcmx5ymm20nq2dfw2g285pjw1bzkxyzn3wndgqqqlq23kyzggn";
-  };
+  src = fetchCodeium version "sha256:08li0a4fdsq7z3ar9yw45ffp6h1qxq7dbpmdyw9rnsr9c6gk01hq";
 
   dontBuild = true;
   dontUnpack = true;
   dontConfigure = true;
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
     gunzip -d -c -f $src > $out/bin/codeium-ls_server_linux_x64
     chmod +x $out/bin/codeium-ls_server_linux_x64
+    runHook postInstall
   '';
+
+  meta = with lib; {
+    description = "Codeium Language Server";
+    homepage = "https://github.com/Exafunction/codeium";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ massimogengarelli ];
+    mainProgram = "codeium-ls_server_linux_x64";
+  };
 }
+
