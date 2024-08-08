66077b3c3275b4d5fa0e076a92072867453c1458
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-08-08 09:00:10 +0200

    style: format

--- home/modules/neovim/files/plugins/coding.lua
+++ home/modules/neovim/files/plugins/coding.lua
@@ -960,16 +960,16 @@ return {
       wk.add({
         { "<C-c>o", group = "overseer" },
         { "<C-c>oT", group = "toggle" },
-        { "<C-c>oTl", "<CMD>OverseerToggle left<cr>", desc = "Toggle left" },
-        { "<C-c>oTr", "<CMD>OverseerToggle right<cr>", desc = "Toggle right" },
-        { "<C-c>oTb", "<CMD>OverseerToggle bottom<cr>", desc = "Toggle bottom" },
       })
     end,
     keys = {
-      { "<C-c>or", [[<cmd>OverseerRun<cr>]], desc = "Overseer Run" },
-      { "<C-c>ot", [[<cmd>OverseerToggle left<cr>]], desc = "Overseer Toggle" },
-      { "<C-c>oq", [[<cmd>OverseerQuickAction<cr>]], desc = "Overseer Quick Action" },
-      { "<C-c>ob", [[<cmd>OverseerBuild<cr>]], desc = "Overseer Build" },
+      { "<C-c>or", "<CMD>OverseerRun<cr>", desc = "Overseer Run" },
+      { "<C-c>ot", "<CMD>OverseerToggle left<cr>", desc = "Overseer Toggle" },
+      { "<C-c>oq", "<CMD>OverseerQuickAction<cr>", desc = "Overseer Quick Action" },
+      { "<C-c>ob", "<CMD>OverseerBuild<cr>", desc = "Overseer Build" },
+      { "<C-c>oTl", "<CMD>OverseerToggle left<cr>", desc = "Toggle left" },
+      { "<C-c>oTr", "<CMD>OverseerToggle right<cr>", desc = "Toggle right" },
+      { "<C-c>oTb", "<CMD>OverseerToggle bottom<cr>", desc = "Toggle bottom" },
     },
   },
 }
