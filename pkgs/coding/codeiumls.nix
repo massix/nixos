7714a7e01cee73b0d8fadb32d8b0f33b054c7444
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-08-24 07:45:20 +0200

    feat(pkgs): update codeium

--- pkgs/coding/codeiumls.nix
+++ pkgs/coding/codeiumls.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 let
   inherit (pkgs) lib;
-  version = "1.8.62";
+  version = "1.12.0";
   fetchCodeium = version: hash: builtins.fetchurl {
     url = "https://github.com/Exafunction/codeium/releases/download/language-server-v${version}/language_server_linux_x64.gz";
     sha256 = hash;
@@ -13,7 +13,7 @@ pkgs.stdenvNoCC.mkDerivation {
 
   nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
 
-  src = fetchCodeium version "sha256:01lbka0rbxjsdj11x652y8x5n3d9f66sgjsisrym2kw9nawx8spv";
+  src = fetchCodeium version "sha256:1chwlr81m3ppjg6z65glyzrlhhj9jp4xb6rqy4p7jwnhg47y34ha";
 
   dontBuild = true;
   dontUnpack = true;
