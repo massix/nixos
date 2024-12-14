7b9db53179f773ff354e5eab5b31b4cead935cf2
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-12-14 08:58:54 +0100

    Revert 1 commits
    6e94481 'feat(elendil): move to Cosmic'

--- system/elendil/configuration.nix
+++ system/elendil/configuration.nix
@@ -50,14 +50,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable COSMIC Desktop Environment
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
-  services.flatpak.enable = true;
-
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.libinput = {
     enable = true;
     touchpad = {
