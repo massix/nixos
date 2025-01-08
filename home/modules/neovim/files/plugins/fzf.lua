8dbffc175a4629b0773ef42aaa293234fb60489a
Massimo Gengarelli <massimo.gengarelli@proton.me>
2025-01-08 09:00:37 +0100

    feat(nvim): add more bindings for FzfLua

--- home/modules/neovim/files/plugins/fzf.lua
+++ home/modules/neovim/files/plugins/fzf.lua
@@ -16,6 +16,8 @@ return {
       { "<leader>/", "<cmd>FzfLua live_grep<cr>", desc = "[FZF] Live Grep" },
       { "<leader>.", "<cmd>FzfLua files<cr>", desc = "[FZF] Files" },
       { "<leader>,", "<cmd>FzfLua buffers<cr>", desc = "[FZF] Buffers" },
+      { "<leader>m", "<cmd>FzfLua marks<cr>", desc = "[FZF] Marks" },
+      { "<leader>r", "<cmd>FzfLua resume<cr>", desc = "[FZF] Resume" },
     },
   },
 }
