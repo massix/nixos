5eb7107a822819d64378d8d1838ca94cc5afd82c
Massimo Gengarelli <massimo.gengarelli@proton.me>
2025-02-01 08:58:08 +0100

    feat(nvim): migrate from fzf-lua to Snacks (and other stuff)

--- home/modules/neovim/files/plugins/snacks.lua
+++ home/modules/neovim/files/plugins/snacks.lua
@@ -19,8 +19,6 @@ local filter_filetypes = function(buf)
     and not is_ignored
 end
 
-local is_dim_enabled = false
-
 return {
 
   {
@@ -39,7 +37,6 @@ return {
 
       Snacks.input.enable()
       Snacks.dim.enable()
-      is_dim_enabled = true
 
       -- Override the default vim.ui.input
       vim.ui.input = Snacks.input.input
@@ -75,12 +72,23 @@ return {
         enabled = true,
         notify_jump = true,
       },
+      picker = {
+        matcher = {
+          frecency = true,
+        },
+        ui_select = true,
+      },
+      explorer = {
+        enabled = true,
+        -- For now I still want to keep Oil.nvim
+        replace_netrw = false,
+      },
       statuscolumn = {
         enabled = true,
-        left = { "git", "mark", "sign" },
-        right = { "fold", "mark" },
+        left = { "fold", "mark" },
+        right = { "sign", "git" },
         folds = {
-          open = true,
+          open = false,
           git_hl = true,
         },
         git = {
@@ -91,57 +99,77 @@ return {
 
       dashboard = { enabled = false },
       bufdelete = { enabled = true },
+      toggle = {
+        enabled = true,
+      },
       dim = {
         enabled = true,
         filter = filter_filetypes,
       },
     },
 
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>p", group = "picker" },
+        { "<leader>pl", group = "lsp" },
+        { "<leader>pg", group = "git" },
+      })
+    end,
+
+    -- stylua: ignore
     keys = {
-      {
-        "<leader>uH",
+      -- Picker keys (quick access)
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Live Grep" },
+      { "<leader>.", function() Snacks.picker.files() end, desc = "Pick files" },
+      { "<leader>;", function() Snacks.picker.git_files() end, desc = "Pick files [Git]" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Pick buffer" },
+      { "<leader>L", function() Snacks.picker.lsp_symbols() end, desc = "Pick LSP Symbols" },
+      { "<leader>m", function() Snacks.picker.marks() end, desc = "Pick Marks" },
+      { "<leader>r", function() Snacks.picker.resume() end, desc = "Resume picker" },
+
+      -- Picker submenu
+      { "<leader>pb", function() Snacks.picker.buffers() end, desc = "Pick buffer" },
+      { "<leader>p/", function() Snacks.picker.grep() end, desc = "Live Grep" },
+      { "<leader>pf", function() Snacks.picker.files() end, desc = "Pick files" },
+      { "<leader>pG", function() Snacks.picker.git_files() end, desc = "Pick files [Git]" },
+      { "<leader>pm", function() Snacks.picker.marks() end, desc = "Pick marks" },
+      { "<leader>pM", function() Snacks.picker.man() end, desc = "Pick man pages" },
+      { "<leader>ph", function() Snacks.picker.help() end, desc = "Pick Help" },
+      { "<leader>pc", function() Snacks.picker.commands() end, desc = "Pick commands" },
+      { "<leader>pH", function() Snacks.picker.command_history() end, desc = "Pick commands" },
+      { "<leader>pH", function() Snacks.picker.registers() end, desc = "Pick registers" },
+      { "<leader>pd", function() Snacks.picker.diagnostics() end, desc = "Pick diagnostics" },
+
+      -- Picker git submenu
+      { "<leader>pgb", function() Snacks.picker.git_branches() end, desc = "Pick git branches" },
+      { "<leader>pgd", function() Snacks.picker.git_diff() end, desc = "Pick git diff" },
+      { "<leader>pgf", function() Snacks.picker.git_files() end, desc = "Pick git files" },
+      { "<leader>pgl", function() Snacks.picker.git_log() end, desc = "Pick git log" },
+
+      -- Picker LSP submenu
+      { "<leader>pls", function() Snacks.picker.lsp_symbols() end, desc = "Pick LSP Symbols" },
+      { "<leader>plD", function() Snacks.picker.lsp_declarations() end, desc = "Pick LSP Declarations" },
+      { "<leader>pld", function() Snacks.picker.lsp_definitions() end, desc = "Pick LSP Definitions" },
+      { "<leader>plt", function() Snacks.picker.lsp_type_definitions() end, desc = "Pick LSP Type Definitions" },
+      { "<leader>plr", function() Snacks.picker.lsp_references() end, desc = "Pick LSP References" },
+      { "<leader>pli", function() Snacks.picker.lsp_implementations() end, desc = "Pick LSP Implementations" },
+
+      -- Explorer keys
+      { "<leader>e", function() Snacks.explorer.open() end, desc = "Open explorer" },
+
+      -- Other keys
+      { "<leader>uH", function() Snacks.notifier.show_history() end, desc = "Snacks History" },
+      { "g[", function() Snacks.words.jump(-1, false) end, desc = "LSP Prev Word" },
+      { "g]", function() Snacks.words.jump(1, false) end, desc = "LSP Next Word" },
+      { "<leader>bd", function() Snacks.bufdelete.delete() end, desc = "Delete buffer" },
+      { "<leader>bD", function() Snacks.bufdelete.other() end, desc = "Delete other buffers" },
+      { "<leader>d",
         function()
-          Snacks.notifier.show_history()
-        end,
-        desc = "Snacks History",
-      },
-      {
-        "g[",
-        function()
-          Snacks.words.jump(-1, false)
-        end,
-        desc = "LSP Prev Word",
-      },
-      {
-        "g]",
-        function()
-          Snacks.words.jump(1, false)
-        end,
-        desc = "LSP Next Word",
-      },
-      {
-        "<leader>bd",
-        function()
-          Snacks.bufdelete.delete()
-        end,
-        desc = "Delete buffer",
-      },
-      {
-        "<leader>bD",
-        function()
-          Snacks.bufdelete.other()
-        end,
-        desc = "Delete other buffers",
-      },
-      {
-        "<leader>d",
-        function()
-          if is_dim_enabled then
+          if Snacks.dim.enabled then
             Snacks.dim.disable()
-            is_dim_enabled = false
           else
             Snacks.dim.enable()
-            is_dim_enabled = true
           end
         end,
         desc = "Toggle Snacks Dim",
