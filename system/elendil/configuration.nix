6cc6f4c4657d2b47aba2649bf53f50fe9d1c4e9b
massix <585424+massix@users.noreply.github.com>
2025-06-07 03:47:02 +0000

    chore: system update

--- system/elendil/configuration.nix
+++ system/elendil/configuration.nix
@@ -59,8 +59,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.gnome.gnome-browser-connector.enable = true;
 
