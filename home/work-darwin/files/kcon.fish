6dd82c95d893a702554700fb908c3b6278197a61
Massimo Gengarelli <massimo.gengarelli@proton.me>
2025-06-11 09:20:00 +0200

    fix: remove black color from kcon script

--- home/work-darwin/files/kcon.fish
+++ home/work-darwin/files/kcon.fish
@@ -4,7 +4,7 @@ set -x KUBECONFIG_FOLDER $HOME/kconfigs
 
 # Small helper function to switch to a given kubeconfig file, residing in the folder ~/kconfigs
 function kcon --description "Switch to a given kubeconfig file in $KUBECONFIG_FOLDER" --argument-names cluster
-    echo -e "Switching to $(set_color --dim black)$KUBECONFIG_FOLDER$(set_color normal)/$(set_color --bold green)$cluster$(set_color normal)"
+    echo -e "Switching to $KUBECONFIG_FOLDER/$(set_color --bold green)$cluster$(set_color normal)"
     set -gx KUBECONFIG $KUBECONFIG_FOLDER/$cluster
 end
 
