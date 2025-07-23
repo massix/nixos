f635e6e25bce3a80958ec2501a1faae18a8643a1
Massimo Gengarelli <massimo.gengarelli@proton.me>
2025-07-23 09:57:26 +0200

    fix(gitlab-ci-ls): use patched derivation
    Fix has already been pushed but did not reach unstable yet.

--- home/modules/neovim/languages/default.nix
+++ home/modules/neovim/languages/default.nix
@@ -11,6 +11,33 @@ let
 
   yamlGenerator = lib.generators.toYAML { };
 
+  # FIXME: remove this once the patch reaches unstable
+  gitlab-ci-ls = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "gitlab-ci-ls";
+    version = "1.1.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "alesbrelih";
+      repo = "gitlab-ci-ls";
+      rev = "${version}";
+      hash = "sha256-0AVi/DyaWh+dCXm/jUf3M63KjobJWCCKHDvm1xGUzCw=";
+    };
+
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-3Ko+vf24dEfu+g4yGA5xY0R0TA9fSWuG398DxhHIVFU=";
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [ pkgs.openssl ];
+
+    meta = with lib; {
+      homepage = "https://github.com/alesbrelih/gitlab-ci-ls";
+      description = "GitLab CI Language Server (gitlab-ci-ls)";
+      license = licenses.mit;
+      maintainers = with maintainers; [ ma27 ];
+      platforms = platforms.unix;
+      mainProgram = "gitlab-ci-ls";
+    };
+  };
+
   language-servers = with pkgs; [
     ansible-language-server-wrapper
     llvmPackages.clang-unwrapped
