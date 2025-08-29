41955faf471f1aae15e211e047b52bf52d315a63
Massimo Gengarelli <massimo.gengarelli@proton.me>
2025-08-29 11:12:18 +0200

    feat(darwin): make it possible to have nested kconfigs

--- home/work-darwin/files/kcon.fish
+++ home/work-darwin/files/kcon.fish
@@ -8,4 +8,13 @@ function kcon --description "Switch to a given kubeconfig file in $KUBECONFIG_FO
     set -gx KUBECONFIG $KUBECONFIG_FOLDER/$cluster
 end
 
-complete --command kcon --no-files --arguments "$(ls $KUBECONFIG_FOLDER)"
+# And of course the complete function has more lines than the function itself.
+function _complete_kcon --description "Tab completion for kcon"
+    for f in (command find $KUBECONFIG_FOLDER -type f)
+        set dir_name (dirname $f | sed 's@'"$KUBECONFIG_FOLDER/"'@@')
+        set cluster_name (basename $f)
+        echo $dir_name/$cluster_name
+    end
+end
+
+complete --command kcon --no-files --arguments "$(_complete_kcon)"
