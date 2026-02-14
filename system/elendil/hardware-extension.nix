4c5b0289d7fbf1c64facdda1fbc693c5ab8ad8c8
Massimo Gengarelli <massimo.gengarelli@proton.me>
2026-02-14 09:31:49 +0100

    feat(thermal): disable PROCHOT at boot, let Intel do its job
    I am wondering if I am damaging my CPU, but so far this is the best
    config I have found, based on the different tests.
    
    I am keeping the thermal-conf file just in case I experience some
    issues.

--- system/elendil/hardware-extension.nix
+++ system/elendil/hardware-extension.nix
@@ -3,15 +3,46 @@
 , pkgs
 , ...
 }:
+let
+  control-prochot = pkgs.writeShellScriptBin "control-prochot" ''
+    #!${pkgs.bash}
+    export PATH="$PATH:${pkgs.msr-tools}/bin"
+
+    current_value="0x$(rdmsr 0x1FC)"
+    disabled="$(printf '0x%x' "$(( current_value & 0xFFFFFFE ))")"
+    enabled="$(printf '0x%x' "$(( current_value | 0xF ))")"
+
+    echo "0x1FC current => $current_value"
+    echo "0x1FC to disable => $disabled"
+    echo "0x1FC to enable => $enabled"
+
+    case "$1" in
+      enable)
+        wrmsr -a 0x1FC $enabled
+        echo "PROCHOT enabled"
+        ;;
+      disable)
+        wrmsr -a 0x1FC $disabled
+        echo "PROCHOT disabled"
+        ;;
+      *)
+        echo "Unknown command $1"
+        exit 255
+        ;;
+    esac
+  '';
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  environment.systemPackages = [ control-prochot ];
+
   boot = {
     initrd = {
       availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-      kernelModules = [ "i915" ];
+      kernelModules = [ "i915" "msr" ];
       systemd.enable = true;
       verbose = false;
     };
@@ -29,7 +60,6 @@
       "rd.udev.log_level=3"
       "udev.log_priority=3"
       "intel_pstate=active"
-      "msr=0x1b0:0x0"
       "processor.max_cstate=1"
     ];
 
@@ -85,8 +115,20 @@
     };
   };
 
-  services.thermald = {
+  systemd.services.disable-prochot = {
     enable = true;
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = ''
+        ${lib.getExe control-prochot} disable
+      '';
+    };
+  };
+
+  services.thermald = {
+    enable = false;
     configFile = ./files/thermal-conf.xml;
   };
 
